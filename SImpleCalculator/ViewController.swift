0095821b08cee8e99d11d9eff680dc76b3505e52
Senno Hananto <sennohananto@gmail.com>
2020-05-04 02:03:24 +0700

    Initial Commit

---
+++ SImpleCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SImpleCalculator
+//
+//  Created by Senno Hananto on 04/05/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
